6ff93c72536f4b843e0fe4abfdace0ccc9006a5a
Osamu Ito <mr.samito@gmail.com>
2022-09-02 15:37:38 -0400

    commit tree 22545fa8d79e7467afac833d36037dfe88c193c5

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+// Code your solution in this file
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers];
+
+
+function createFareMultiplier(multiplyx){
+    return function(x){
+       return x*multiplyx
+    }
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, returnNewDrivers) {
+    return returnNewDrivers(drivers);
+  };
